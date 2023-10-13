540b1df29f083e66bdd8f4b128c1fc6c7c262185
joon jeong <juny.jeong@gmail.com>
2023-10-14 08:56:07 +1300

    update product db setting.

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -30,14 +30,22 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
-    connection: process.env.DATABASE_URL,
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: path.join(__dirname, 'dev.sqlite3'),
+    },
     pool: {
-      min: 2,
-      max: 10,
+      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
     },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
+    // client: 'postgresql',
+    // connection: process.env.DATABASE_URL,
+    // pool: {
+    //   min: 2,
+    //   max: 10,
+    // },
+    // migrations: {
+    //   tableName: 'knex_migrations',
+    // },
   },
 }
