883ae28340209c45d3e18090c55e4081721d9e46
joon jeong <juny.jeong@gmail.com>
2023-10-18 11:29:58 +1300

    update knexfile for production setting

--- server/db/knexfile.js
+++ server/db/knexfile.js
@@ -30,22 +30,17 @@ module.exports = {
   },
 
   production: {
-    client: 'sqlite3',
-    useNullAsDefault: true,
+    client: 'pg',
     connection: {
-      filename: path.join(__dirname, 'dev.sqlite3'),
+      connectionString: process.env.DATABASE_URL,
+      host: process.env.PGHOST,
+      port: process.env.PGPORT,
+      user: process.env.PGUSER,
+      password: process.env.PGPASSWORD,
+      database: process.env.PGDATABASE,
     },
-    pool: {
-      afterCreate: (conn, cb) => conn.run('PRAGMA foreign_keys = ON', cb),
+    migrations: {
+      tableName: 'knex_migrations',
     },
-    // client: 'postgresql',
-    // connection: process.env.DATABASE_URL,
-    // pool: {
-    //   min: 2,
-    //   max: 10,
-    // },
-    // migrations: {
-    //   tableName: 'knex_migrations',
-    // },
   },
 }
